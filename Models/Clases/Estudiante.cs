a06c7a0a4d12d5ab3ea95c89065455879173cdd3
Manuuuu777 <vc23019@ues.edu.sv>
2025-11-21 13:58:58 -0600

    Subida inicial del proyecto veterinaria (con .gitignore)

---
+++ Models/Clases/Estudiante.cs
@@ -0,0 +1,11 @@
+﻿namespace proyecto_ESD.Models.Clases
+{
+    public class Estudiante
+    {
+        public int IdEstudiante { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public DateTime FechaNacimiento { get; set; }
+        public string Email { get; set; } = string.Empty;
+    }
+
+}
